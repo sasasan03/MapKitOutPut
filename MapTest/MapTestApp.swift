d213b6430962bc9efec3eb8326079e7bee427e98
sasasan03 <sako.hiro62@gmail.com>
2023-01-31 18:43:07 +0900

    Initial Commit

---
+++ MapTest/MapTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapTestApp.swift
+//  MapTest
+//
+//  Created by sako0602 on 2023/01/31.
+//
+
+import SwiftUI
+
+@main
+struct MapTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
